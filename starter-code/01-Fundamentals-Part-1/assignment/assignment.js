a10a6be3f7a5d4788f066bafc92799aeabe93bdd
zhaley720 <thez.haley@yahoo.com>
2025-11-27 14:50:54 -0500

    finished first section

---
+++ starter-code/01-Fundamentals-Part-1/assignment/assignment.js
@@ -0,0 +1,46 @@
+
+// Values and Variables
+// let country = "United States of America";
+let continent = "North America";
+// let population = 347000000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data Types
+// let isIsland = false;
+// let population = 347000000;
+// let country = "United States of America";
+// let language;
+
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
+
+// let, const, and var
+const isIsland = false;
+let population = 347000000;
+const country = "United States of America";
+const language = "English";
+// isIsland = true;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// Basic Operators
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+const finlandPop = 6000000
+console.log(population > finlandPop);
+
+const avgPop = 33000000;
+console.log(population < avgPop);
+
+let description = `${country} is in ${continent}, and it's ${population} people speak ${language}`;
---
+++ starter-code/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert("javascript is fun!");
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("jonas");
+// console.log(23);
+
+// let firstName = "Jonas";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+
+// const favorite = prompt("what's your favorite number?");
+// console.log(favorite);
+// console.log(typeof favorite);
+// const favoriteNum = Number(favorite);
+// console.log(favoriteNum);
+// console.log(typeof favoriteNum);
+
+const favorite = Number(prompt("what is your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
