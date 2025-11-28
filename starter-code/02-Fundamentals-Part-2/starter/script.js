4f312ef37b2ddb8e2dbea259eef237e5b3f5d905
zhaley720 <thez.haley@yahoo.com>
2025-11-28 17:11:36 -0500

    finished section 2 of the course, spent over 2 hours coding

--- starter-code/02-Fundamentals-Part-2/starter/script.js
+++ starter-code/02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // function logger() {
 //     console.log(`my name is zak`);
 // }
@@ -95,3 +97,190 @@
 // const firstName = `jonas`;
 // const jonas = [firstName, `schmedtmann`, 2037 - 1991, `teacher`, friends];
 // console.log(jonas);
+
+
+
+// const friends = ['michael', 'steven', 'peter'];
+// const newLength = friends.push('jay');
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift('john');
+// console.log(friends);
+
+// friends.pop();
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+
+// friends.shift();
+// console.log(friends);
+
+// console.log(friends.indexOf('steven'));
+// console.log(friends.indexOf('bob'));
+
+// friends.push(23);
+// console.log(friends.includes('steven'));
+// console.log(friends.includes('bob'));
+// console.log(friends.includes(23));
+
+// if (friends.includes('steven')) {
+//     console.log('you have a friend called steven');
+// }
+
+
+
+// const jonas = {
+//     firstName: `jonas`,
+//     lastName: `schmedtmann`,
+//     age: 2037 - 1991,
+//     job: `teacher`,
+//     friends: [`michael`, `peter`, `steven`]
+// };
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+// console.log(jonas[`lastName`]);
+
+// const nameKey = `Name`;
+// console.log(jonas[`first` + nameKey]);
+// console.log(jonas[`last` + nameKey]);
+
+// const interestedIn = prompt(`what do you want to know about jonas? choose between firstName, lastName, age, job, and friends`);
+
+// if (jonas[interestedIn]) {
+//     console.log(jonas[interestedIn]);
+// } else {
+//     console.log`wrong request!`
+// }
+
+// jonas.location = `portugal`;
+// jonas[`twitter`] = `@jonasschmedtman`;
+// console.log(jonas);
+
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+
+
+// const jonas = {
+//     firstName: `jonas`,
+//     lastName: `schmedtmann`,
+//     birthYear: 1991,
+//     job: `teacher`,
+//     friends: [`michael`, `peter`, `steven`],
+//     hasDriversLicense: true,
+
+//     // calcAge: function(birthYear) {
+//     //     return 2037 - birthYear;
+//     // }
+
+//     calcAge: function() {
+//         // console.log(this);
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+
+//     getSummary: function() {
+//         return `${this.firstName} is a ${this.calcAge()} year old ${this.job}, and he has ${this.hasDriversLicense ? `a` : `no`} driver's license`
+//     }
+// };
+
+// // console.log(jonas.calcAge(1991));
+// // console.log(jonas[`calcAge`](1991));
+
+// console.log(jonas.calcAge());
+
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age); 
+
+// console.log(jonas.getSummary());
+
+
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`lifting weights repetition ${rep}`);
+// }
+
+
+
+// const jonas = [
+//     `jonas`,
+//     `schmedtmann`,
+//     2037 - 1991,
+//     `teacher`,
+//     [`michael`, `peter`, `steven`],
+//     true
+// ];
+
+// const types = [];
+
+// for (let i = 0; i < jonas.length; i++) {
+//     console.log(jonas[i], typeof jonas[i]);
+
+//     types.push(typeof jonas[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2037 - years[i]);
+// }
+
+// console.log(ages);
+
+// // continue and break
+// console.log(`--- ONLY STRINGS ---`);
+// for (let i = 0; i < jonas.length; i++) {
+//     if (typeof jonas[i] !== `string`) continue;
+
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log(`--- BREAK WITH NUMBER ---`);
+// for (let i = 0; i < jonas.length; i++) {
+//     if (typeof jonas[i] === `number`) break;
+
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+
+
+
+// const jonas = [
+//     `jonas`,
+//     `schmedtmann`,
+//     2037 - 1991,
+//     `teacher`,
+//     [`michael`, `peter`, `steven`],
+// ];
+
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//     console.log(i, jonas[i]);
+// }
+
+// for (let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`----- Starting exercise ${exercise}`);
+
+//     for (let rep = 1; rep < 6; rep++) {
+//         console.log(`exercise ${exercise}: lifting weight repetition ${rep}`);
+//     }
+// }
+
+
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+    console.log(`you rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`loop is about to end...`);
+}
