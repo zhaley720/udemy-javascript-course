f216021aac5d2d0bab6967a1861e3cd2ad283650
zhaley720 <thez.haley@yahoo.com>
2025-11-28 02:41:50 -0500

    worked a little bit more on it, but i'm tired now

--- starter-code/02-Fundamentals-Part-2/starter/script.js
+++ starter-code/02-Fundamentals-Part-2/starter/script.js
@@ -74,3 +74,24 @@
 
 
 
+// const friend1= `michael`;
+// const friend2 = `steven`;
+// const friend3 = `peter`;
+
+// const friends = [`michael`, `steven`, `peter`];
+// console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = `jay`;
+// console.log(friends);
+
+// const firstName = `jonas`;
+// const jonas = [firstName, `schmedtmann`, 2037 - 1991, `teacher`, friends];
+// console.log(jonas);
