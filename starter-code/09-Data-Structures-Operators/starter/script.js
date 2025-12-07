5eb2a94aaf4487721a79e01e1a1a26002333a217
zhaley720 <thez.haley@yahoo.com>
2025-12-06 23:38:13 -0500

    spent 1 hour coding

--- starter-code/09-Data-Structures-Operators/starter/script.js
+++ starter-code/09-Data-Structures-Operators/starter/script.js
@@ -44,4 +44,74 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(starterIdx, mainIdx) {
+    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+
+  orderDelivery: function({ starterIndex = 1, mainIndex = 0, time = `20:00`, address }) {
+    console.log(`order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
 };
+
+
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// // Nested Destructuring
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+// // console.log(i, j);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// // default values
+// // const [p, q, r] = [8, 9];
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
+
+
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+const { fri: { open: o, close: c } } = openingHours;
+console.log(o, c);
+
+restaurant.orderDelivery({
+  time: `22:30`,
+  address: `Via del Sole, 21`,
+  mainIndex: 2,
+  starterIndex: 2
+});
+
+restaurant.orderDelivery({
+  address: `Via del Sole, 21`,
+  starterIndex: 1
+})
