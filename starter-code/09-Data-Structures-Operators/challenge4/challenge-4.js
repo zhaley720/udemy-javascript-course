5fddedf6c4b353bf188effc6b9e144e3605fda7f
Zachary <thez.haley@yahoo.com>
2025-12-23 19:06:59 -0500

    finished section 9

--- starter-code/09-Data-Structures-Operators/challenge4/challenge-4.js
+++ starter-code/09-Data-Structures-Operators/challenge4/challenge-4.js
@@ -39,6 +39,8 @@ GOOD LUCK 😀
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
+
+// my solution
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
   const rows = text.split('\n');
@@ -52,6 +54,17 @@ document.querySelector('button').addEventListener('click', function () {
     const camelCase = `${cur[0]}${cur[1].replace(cur[1][0], cur[1][0].toUpperCase())}`
     console.log(camelCase.padEnd(20, ' ') + '✅'.repeat(i + 1));
   }
-
-  
 });
+
+
+// //course solution
+// document.querySelector('button').addEventListener('click', function() {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
--- starter-code/09-Data-Structures-Operators/starter/script.js
+++ starter-code/09-Data-Structures-Operators/starter/script.js
@@ -1,12 +1,11 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
   
-  // Data needed for first part of the section
-  const weekdays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`];
+// Data needed for first part of the section
+const weekdays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`];
 
 const openingHours = {
   [weekdays[3]]: {
@@ -580,53 +579,71 @@ const rest2 = {
 
 
 
-console.log(`a+very+nice+string`.split('+'));
-console.log('Jonas Schmedtmann'.split(' '));
+// console.log(`a+very+nice+string`.split('+'));
+// console.log('Jonas Schmedtmann'.split(' '));
 
-const [firstName, lastname] = 'Jonas Schmedtmann'.split(' ');
+// const [firstName, lastname] = 'Jonas Schmedtmann'.split(' ');
 
-const newName = ['Mr.', firstName, lastname.toUpperCase()].join(' ');
-console.log(newName);
+// const newName = ['Mr.', firstName, lastname.toUpperCase()].join(' ');
+// console.log(newName);
 
-const capitalizeName = function(name) {
-  const names = name.split(' ');
-  const namesUpper = [];
+// const capitalizeName = function(name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
 
-  for (const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
 
-  console.log(namesUpper.join(' '));
-};
+//   console.log(namesUpper.join(' '));
+// };
 
-capitalizeName('jessica ann smith davis');
-capitalizeName('jonas schmedtmann');
-capitalizeName('zachary haley');
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('jonas schmedtmann');
+// capitalizeName('zachary haley');
 
-// padding
-const message = 'Go to gate 23!';
-console.log(message.padStart(25, '+').padEnd(35, '+'));
-console.log('Jonas'.padStart(25, '+').padEnd(35, '+'));
+// // padding
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(25, '+').padEnd(35, '+'));
+// console.log('Jonas'.padStart(25, '+').padEnd(35, '+'));
 
-const maskCreditCard = function(number) {
-  const str = number + '';
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
+// const maskCreditCard = function(number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// }
+
+// console.log(maskCreditCard(77826915));
+// console.log(maskCreditCard(54497542494348484612));
+// console.log(maskCreditCard('6784662464842498464642468464333'));
+
+// // repeat
+// const message2 = 'bad weather... all departures delayed... ';
+// console.log(message2.repeat(5));
+
+// const planesInLine = function(n) {
+//   console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+// };
+
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
+
+
+
+// const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25
+// +_Arrival;bru0943384722;fao93766109;11:45
+// +_Delayed_Arrival;hel7439299980;fao93766109;12:05
+// +_Departure;fao93766109;lis2323639855;12:30';
+
+// console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  // console.log(flight.split(';'));
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(55);
+  console.log(output);
 }
-
-console.log(maskCreditCard(77826915));
-console.log(maskCreditCard(54497542494348484612));
-console.log(maskCreditCard('6784662464842498464642468464333'));
-
-// repeat
-const message2 = 'bad weather... all departures delayed... ';
-console.log(message2.repeat(5));
-
-const planesInLine = function(n) {
-  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
-};
-
-planesInLine(5);
-planesInLine(3);
-planesInLine(12);
